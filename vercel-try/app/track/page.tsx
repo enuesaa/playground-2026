18f5574645c399102928ac89e657e93d8796c258
nua <enuesaa@gmail.com>
2026-01-17 14:52:52 +0900

    feat: upstash redis へ接続してみる

--- vercel-try/app/track/page.tsx
+++ vercel-try/app/track/page.tsx
@@ -1,6 +1,6 @@
 import { exampleFlag } from '../../flags'
-import { track } from '@vercel/analytics/server';
-import { reportValue } from 'flags';
+import { track } from '@vercel/analytics/server'
+import { reportValue } from 'flags'
 import { VercelToolbar } from '@vercel/toolbar/next'
 
 // this is on server
@@ -8,7 +8,7 @@ export default async function Page() {
   const example = await exampleFlag()
 
   // see https://vercel.com/docs/feature-flags/integrate-with-web-analytics
-  reportValue('example-flag', example);
+  reportValue('example-flag', example)
   track('My Event', {}, { flags: ['example-flag'] })
 
   // 実際には staff の認可を前段階でして、もし staff であればツールバーを表示するイメージ
@@ -18,5 +18,6 @@ export default async function Page() {
     <>
       <div>Flag {example ? 'on' : 'off'}</div>
       {isStaff && <VercelToolbar />}
-    </>)
+    </>
+  )
 }
