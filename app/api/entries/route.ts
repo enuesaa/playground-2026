1acd449e0a20254036af6f797fef3d1e9d8b5946
nua <enuesaa@gmail.com>
2026-01-17 19:02:17 +0900

    feat: save entries

--- app/api/entries/route.ts
+++ app/api/entries/route.ts
@@ -1,6 +1,33 @@
-import entries from './data.json'
+// import entries from './data.json'
 import { NextResponse } from 'next/server'
+import { z } from 'zod'
+import { Redis } from '@upstash/redis'
+import { ulid } from "ulid";
+
+const redis = Redis.fromEnv()
 
 export async function GET() {
-  return NextResponse.json(entries)
+  // return NextResponse.json(entries)
+}
+
+const schema = z.object({
+  entries: z.array(z.object({
+    title: z.string().min(1),
+    link: z.string().min(1),
+    comments: z.string().min(1),
+    imageUrl: z.string(),
+    popularity: z.number(),
+  })),
+})
+
+export async function POST(request: Request) {
+  const body = schema.safeParse(await request.json())
+  if (!body.success) {
+    return NextResponse.json({ error: 'invalid request' }, { status: 400 })
+  }
+
+  for (const entry of body.data.entries) {
+    await redis.set(`entry-${ulid()}`, JSON.stringify(entry))
+  }
+  return NextResponse.json({}, {status: 200})
 }
