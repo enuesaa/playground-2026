4def338c24a8df959d4d9ab8521bb951bfedfc3b
nua <enuesaa@gmail.com>
2026-01-29 17:31:21 +0900

    feat: 横方向に文字を表示できるようにした

--- m5sticks3-try/src/main.cpp
+++ m5sticks3-try/src/main.cpp
@@ -2,7 +2,14 @@
 
 void setup() {
   M5.begin();
-  M5.Lcd.println("a");
+
+  // 回転
+  M5.Display.setRotation(1);
+
+  M5.Display.setTextSize(8);
+  M5.Display.setCursor(20, 20);
+
+  M5.Lcd.println("aaa");
 }
 
 void loop() {}
