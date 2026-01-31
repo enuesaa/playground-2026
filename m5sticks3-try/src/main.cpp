4672d2b898cd9ff05f0ea5eddccdc87dd45b9df7
nua <enuesaa@gmail.com>
2026-01-31 19:01:10 +0900

    feat: m5sticks3 でボタンをクリックしたときに検出できるようにした

--- m5sticks3-try/src/main.cpp
+++ m5sticks3-try/src/main.cpp
@@ -6,14 +6,27 @@ void setup() {
   // 回転
   M5.Display.setRotation(1);
 
-  M5.Display.setTextSize(8);
+  M5.Display.setTextSize(5);
   M5.Display.setCursor(20, 20);
 
   M5.Lcd.println("aaa");
 
-  delay(7000);
-  M5.Display.sleep();
-  M5.Power.powerOff();
+  // delay(7000);
+  // M5.Display.sleep();
+  // M5.Power.powerOff();
 }
 
-void loop() {}
+// see https://docs.m5stack.com/ja/arduino/m5sticks3/button
+void loop() {
+  M5.delay(1);
+  M5.update();
+
+  // 正面についているボタン
+  if (M5.BtnA.isPressed()) {
+    M5.Lcd.println("ButtonA clicked");
+  }
+  // 右脇のボタン
+  if (M5.BtnB.isPressed()) {
+    M5.Lcd.println("ButtonB clicked");
+  }
+}
