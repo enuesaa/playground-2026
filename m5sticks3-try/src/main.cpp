ac7349432317bbaed5cf307fb8f274a610afb076
nua <enuesaa@gmail.com>
2026-02-08 16:40:19 +0900

    feat: m5stack でフォントを変える

--- m5sticks3-try/src/main.cpp
+++ m5sticks3-try/src/main.cpp
@@ -1,26 +1,40 @@
 #include <M5Unified.h>
 
+uint16_t greenColor = M5.Lcd.color565(33, 143, 105);
+
 void setup() {
   M5.begin();
 
   // 回転
   M5.Display.setRotation(1);
 
-  M5.Display.setTextSize(5);
-  M5.Display.setCursor(20, 20);
+  // クリア
+  M5.Lcd.clear();
+  M5.Lcd.fillScreen(greenColor);
+  M5.Lcd.setTextColor(WHITE, greenColor);
 
+  // 文字表示
+  // https://qiita.com/tomoto335/items/9bb4427d6eb8e993b185
+  // https://lang-ship.com/reference/unofficial/M5StickC/Tips/M5Display/
+  M5.Display.setCursor(15, 5);
+  M5.Display.setTextSize(1);
+  M5.Display.setTextFont(7); // 数字フォント
+  M5.Lcd.println("012");
+
+  M5.Display.setCursor(15, 60);
+  M5.Display.setTextSize(3);
+  M5.Display.setTextFont(1); // テキストフォント
   M5.Lcd.println("aaa");
 
-  delay(7000);
-  M5.Power.powerOff();
+  // delay(7000);
+  // M5.Power.powerOff();
 }
 
 // see https://docs.m5stack.com/ja/arduino/m5sticks3/button
 void loop() {
-  M5.delay(1);
   M5.update();
 
-  // 正面についているボタン
+  // 正面についているボタン (ボタンを押している最中か)
   if (M5.BtnA.isPressed()) {
     M5.Lcd.println("ButtonA clicked");
   }
@@ -28,4 +42,6 @@ void loop() {
   if (M5.BtnB.isPressed()) {
     M5.Lcd.println("ButtonB clicked");
   }
+
+  M5.delay(10);
 }
