188fa85777e97a522bb4a40d82a9b15d5696f6ba
nua <enuesaa@gmail.com>
2026-02-14 17:00:16 +0900

    feat: wav file を流せるようにした

--- m5stack-atoms3r-try/src/main.cpp
+++ m5stack-atoms3r-try/src/main.cpp
@@ -1,31 +1,35 @@
 #include <M5Unified.h>
-#include <NTPClient.h>
-#include <WiFi.h>
-
-WiFiUDP ntpUDP;
-NTPClient ntp(ntpUDP);
+#include "SPIFFS.h"
 
 void setup() {
   M5.begin();
-
-  WiFi.begin("", "");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-  }
-  ntp.begin();
-  ntp.update();
+  // M5.Speaker.setVolume(50);
+  // M5.Speaker.setAllChannelVolume(50);
 
   M5.Speaker.tone(800, 200);
   M5.delay(500);
+
+  if (!SPIFFS.begin(true)) {
+    return;
+  }
+  if (!SPIFFS.exists("/hello.wav")) {
+    M5.Speaker.tone(800, 200);
+    M5.delay(500);
+    return;
+  }
+
+  File wavFile = SPIFFS.open("/hello.wav");
+  if (!wavFile) {
+    return;
+  }
+  static uint8_t buf[46000];
+  int len = wavFile.read(buf, sizeof(buf));
+  M5.Speaker.playWav(buf, len);
+    
+  while (M5.Speaker.isPlaying()) {
+    delay(1);
+  }
 }
 
 void loop() {
-  M5.update();
-
-  // 正面のボタン
-  if (M5.BtnA.wasPressed()) {
-    M5.Speaker.tone(800, 200);
-    M5.delay(500);
-  }
-  M5.delay(10);
 }
