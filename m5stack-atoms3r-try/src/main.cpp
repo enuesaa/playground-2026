1796a14159ac34e246ca5faf0d49240604353c2a
nua <enuesaa@gmail.com>
2026-02-14 17:27:10 +0900

    feat: embedded WAV file

--- m5stack-atoms3r-try/src/main.cpp
+++ m5stack-atoms3r-try/src/main.cpp
@@ -1,31 +1,15 @@
 #include <M5Unified.h>
-#include "SPIFFS.h"
+#include "hello.h"
 
 void setup() {
   M5.begin();
-  // M5.Speaker.setVolume(50);
-  // M5.Speaker.setAllChannelVolume(50);
+  M5.Speaker.setVolume(100);
+  M5.Speaker.setAllChannelVolume(100);
 
   M5.Speaker.tone(800, 200);
   M5.delay(500);
 
-  if (!SPIFFS.begin(true)) {
-    return;
-  }
-  if (!SPIFFS.exists("/hello.wav")) {
-    M5.Speaker.tone(800, 200);
-    M5.delay(500);
-    return;
-  }
-
-  File wavFile = SPIFFS.open("/hello.wav");
-  if (!wavFile) {
-    return;
-  }
-  static uint8_t buf[46000];
-  int len = wavFile.read(buf, sizeof(buf));
-  M5.Speaker.playWav(buf, len);
-    
+  M5.Speaker.playWav(hello_wav, hello_wav_len);
   while (M5.Speaker.isPlaying()) {
     delay(1);
   }
