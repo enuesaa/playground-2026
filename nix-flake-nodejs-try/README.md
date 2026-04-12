912cf7d05bffde34998b944bbc2d05aad6c1afc9
nua <enuesaa@gmail.com>
2026-04-12 15:34:47 +0900

    feat: try nix flake

---
+++ nix-flake-nodejs-try/README.md
@@ -0,0 +1,12 @@
+# nix flake
+
+- nix の依存パッケージマネージャのことを nix flake というらしい
+- flake.nix に node.js とかを書くと、それをインストールしてくれる
+- まあ便利ではある
+  - がnixそのものの導入コストが高い
+  - sudoをたくさん要求される
+  - 個人的にはちょっと怖い
+
+## Links
+- https://search.nixos.org/packages?channel=25.11&query=go
+- https://syu-m-5151.hatenablog.com/entry/2025/12/18/111500
---
+++ nix-flake-nodejs-try/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "my portable dev env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs
+        ];
+      };
+    };
+}
