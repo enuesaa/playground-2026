1ee2a81ee65c7f9336bd8bf80667d1d442fb6fa0
nua <enuesaa@gmail.com>
2026-05-02 17:40:24 +0900

    feat: 無理やりmqttで送信してみる

--- .devdev/m5stack-mic-record/src/main.cpp
+++ .devdev/m5stack-mic-record/src/main.cpp
@@ -13,14 +13,33 @@ static int16_t rec_buffer[16000 * 3];
 void setup() {
   M5.begin();
   M5.Mic.begin();
+  M5.Lcd.setTextSize(3);
   M5.Speaker.setVolume(150);
 
-  M5.Lcd.setTextSize(3);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    M5.delay(1000);
+  }
+  M5.Display.println("wifi ok");
+
+  net.setCACert(AWSIOT_ROOT_CA);
+  net.setCertificate(AWSIOT_CERTIFICATE);
+  net.setPrivateKey(AWSIOT_PRIVATE_KEY);
+  client.setServer(AWSIOT_ENDPOINT, 8883);
+
+  while (!client.connected()) {
+    if (!client.connect(AWSIOT_THING_ID)) {
+      M5.delay(1000);
+    }
+  }
+  M5.Display.println("MQTT OK");
+  client.publish("sdk/test/python", "hello");
+
   M5.Display.println("Start in 2 sec...");
   M5.delay(2000);
 
   M5.Display.println("Recording 3 sec...");
-  int is = M5.Mic.record(rec_buffer, 16000 * 3, 16000);
+  M5.Mic.record(rec_buffer, 16000 * 3, 16000);
   M5.delay(3000);
   M5.Display.println("Record done");
 
@@ -28,8 +47,13 @@ void setup() {
   M5.Display.println("Playing...");
   M5.Speaker.playRaw((const int16_t*)rec_buffer, 16000 * 3, 16000);
   M5.Display.println("Done");
+
+  bool ok = client.publish("audio/raw", (uint8_t*)rec_buffer, 16000 * 3);
+  if (ok) {
+    M5.Display.println("Publish OK");
+  } else {
+    M5.Display.println("Publish FAILED");
+  }
 }
 
-void loop() {
-  M5.delay(1000);
-}
+void loop() {}
