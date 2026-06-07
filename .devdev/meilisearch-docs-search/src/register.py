3eee3ae6bd8e3e2611033d81327e27b7cb332e3c
nua <enuesaa@gmail.com>
2026-06-07 16:29:16 +0900

    feat: meilisearch に docs をつっこんで検索してみる

---
+++ .devdev/meilisearch-docs-search/src/register.py
@@ -0,0 +1,31 @@
+import pathlib
+import meilisearch
+
+client = meilisearch.Client("http://localhost:7700", "masterKey")
+index = client.index("projects")
+
+docs = []
+
+for readme in pathlib.Path("../../").glob("*/README.md"):
+    print(readme.parent.name)
+    docs.append(
+        {
+            "id": readme.parent.name,
+            "project": readme.parent.name,
+            "path": str(readme),
+            "content": readme.read_text(),
+        }
+    )
+index.add_documents(docs)
+
+# index.update_settings(
+#     {
+#         "embedders": {
+#             "default": {
+#                 "source": "openAi",
+#                 "apiKey": "...",
+#                 "model": "text-embedding-3-small",
+#             }
+#         }
+#     }
+# )
---
+++ .devdev/meilisearch-docs-search/src/search.py
@@ -0,0 +1,17 @@
+import meilisearch
+
+client = meilisearch.Client("http://localhost:7700", "masterKey")
+index = client.index("projects")
+
+result = index.search(
+    "node.js",
+    # {
+    #     "hybrid": {
+    #         "embedder": "default",
+    #         "semanticRatio": 0.5,
+    #     },
+    # },
+)
+
+for hit in result["hits"]:
+    print(hit["project"])
