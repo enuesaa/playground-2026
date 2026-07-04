ba43dd566ae731d6042694517d168e1028f7abdb
nua <enuesaa@gmail.com>
2026-07-04 19:49:07 +0900

    feat: cloud run の認証は不要

--- .devdev/google-cloud-vpc-db-app-terraform/modules/app/cloudrun.tf
+++ .devdev/google-cloud-vpc-db-app-terraform/modules/app/cloudrun.tf
@@ -2,6 +2,8 @@ resource "google_cloud_run_v2_service" "app" {
   name     = "${var.identifier}-service"
   project  = var.project_id
   location = var.region
+  invoker_iam_disabled = true
+  deletion_protection = false
 
   scaling {
     manual_instance_count = 0
@@ -10,7 +12,17 @@ resource "google_cloud_run_v2_service" "app" {
 
   template {
     containers {
-      image = "us-docker.pkg.dev/cloudrun/container/hello" # default
+      # image = "us-docker.pkg.dev/cloudrun/container/hello" # default
+      image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.main.repository_id}/app:latest"
+
+      env {
+        name  = "DB_HOST"
+        value = google_alloydb_instance.primary.ip_address
+      }
+      env {
+        name = "DB_PASSWORD"
+        value = var.alloydb_password # this is not good
+      }
     }
 
     vpc_access {
