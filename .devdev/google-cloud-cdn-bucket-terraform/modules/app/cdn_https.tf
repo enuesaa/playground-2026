f283fa9cfe767c45f58d0f94245d76543c1b5773
nua <enuesaa@gmail.com>
2026-07-05 18:05:50 +0900

    feat: creating cert

--- .devdev/google-cloud-cdn-bucket-terraform/modules/app/cdn_https.tf
+++ .devdev/google-cloud-cdn-bucket-terraform/modules/app/cdn_https.tf
@@ -1,24 +1,22 @@
-resource "google_compute_target_https_proxy" "main" {
-  project          = var.project_id
-  name             = "${var.identifier}-https"
-  url_map          = google_compute_url_map.main.id
-  ssl_certificates = [google_compute_managed_ssl_certificate.main.id]
-}
+# resource "google_compute_target_https_proxy" "main" {
+#   project          = var.project_id
+#   name             = "${var.identifier}-https"
+#   url_map          = google_compute_url_map.main.id
+#   ssl_policy = google_compute_ssl_policy.main.name
+#   certificate_map = "//certificatemanager.googleapis.com/${google_certificate_manager_certificate_map.main.id}"
+# }
 
-resource "google_compute_global_forwarding_rule" "https" {
-  project               = var.project_id
-  name                  = "${var.identifier}-https"
-  target                = google_compute_target_https_proxy.main.id
-  port_range            = "443"
-  ip_address            = google_compute_global_address.main.id
-  load_balancing_scheme = "EXTERNAL"
-}
+# resource "google_compute_ssl_policy" "main" {
+#   name            = "ssl-policy"
+#   profile         = "MODERN"
+#   min_tls_version = "TLS_1_2"
+# }
 
-resource "google_compute_managed_ssl_certificate" "main" {
-  project = var.project_id
-  name    = var.identifier
-
-  managed {
-    domains = [var.domain]
-  }
-}
+# resource "google_compute_global_forwarding_rule" "https" {
+#   project               = var.project_id
+#   name                  = "${var.identifier}-https"
+#   target                = google_compute_target_https_proxy.main.id
+#   port_range            = "443"
+#   ip_address            = google_compute_global_address.main.id
+#   load_balancing_scheme = "EXTERNAL"
+# }
---
+++ .devdev/google-cloud-cdn-bucket-terraform/modules/app/cdn_https_cert.tf
@@ -0,0 +1,36 @@
+# これは Managed SSL というあんまり良くない発行の仕方らしい。切り替えの時にダウンタイムが発生しうる
+# see https://zenn.dev/glassonion1/articles/2824399b7759d8
+# resource "google_compute_managed_ssl_certificate" "main" {
+#   project = var.project_id
+#   name    = var.identifier
+#   managed {
+#     domains = [var.domain]
+#   }
+# }
+
+# 証明書
+resource "google_certificate_manager_certificate" "main" {
+  name = var.identifier
+
+  managed {
+    domains = [google_certificate_manager_dns_authorization.main.domain]
+    dns_authorizations = [google_certificate_manager_dns_authorization.main.id]
+  }
+}
+
+resource "google_certificate_manager_dns_authorization" "main" {
+  name   = var.identifier
+  domain = var.domain
+}
+
+# 証明書マップ
+resource "google_certificate_manager_certificate_map" "main" {
+  name = var.identifier
+}
+
+resource "google_certificate_manager_certificate_map_entry" "main" {
+  name = var.identifier
+  map  = google_certificate_manager_certificate_map.main.name
+  hostname = google_certificate_manager_dns_authorization.main.domain
+  certificates = [google_certificate_manager_certificate.main.id]
+}
