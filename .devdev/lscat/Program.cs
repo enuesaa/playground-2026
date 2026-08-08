d80a7d734be68f9904c77a9308b654d64d60cf96
nua <enuesaa@gmail.com>
2026-08-08 19:34:39 +0900

    feat: select file

--- .devdev/lscat/Program.cs
+++ .devdev/lscat/Program.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.IO;
+using Sharprompt;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("> ");
-        string? path = Console.ReadLine();
+        string dir = Directory.GetCurrentDirectory();
+        var files = Directory.GetFiles(dir);
 
-        if (string.IsNullOrWhiteSpace(path))
+        if (files.Length == 0)
         {
-            Console.Error.WriteLine("please enter text.");
+            Console.Error.WriteLine("There are no files here.");
             Environment.Exit(1);
         }
 
+        string selected = Prompt.Select("Please select a file", items: files);
+        // Console.Write("> ");
+        // string? path = Console.ReadLine();
+
+        // if (string.IsNullOrWhiteSpace(path))
+        // {
+        //     Console.Error.WriteLine("please enter text.");
+        //     Environment.Exit(1);
+        // }
+
         try
         {
-            using var reader = new StreamReader(path!);
+            using var reader = new StreamReader(selected!);
             string? line;
             while ((line = reader.ReadLine()) != null)
             {
@@ -25,7 +36,7 @@ class Program
         }
         catch (FileNotFoundException)
         {
-            Console.Error.WriteLine($"file not found: {path}");
+            Console.Error.WriteLine($"file not found: {selected}");
             Environment.Exit(1);
         }
         catch (Exception ex)
