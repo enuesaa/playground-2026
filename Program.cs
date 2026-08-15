2370d0251492f69ed8d40542e557509b53afcbfc
nua <enuesaa@gmail.com>
2026-08-15 17:39:37 +0900

    feat: try to connect sql server

--- Program.cs
+++ Program.cs
@@ -4,4 +4,14 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello");
 app.MapGet("/aaa", () => "aaa");
 
+app.MapGet("/db-test", async () =>
+{
+    var connStr = builder.Configuration.GetConnectionString("DefaultConnection")
+        ?? Environment.GetEnvironmentVariable("SQLCONNSTR_DefaultConnection");
+
+    using var conn = new Microsoft.Data.SqlClient.SqlConnection(connStr);
+    await conn.OpenAsync();
+    return "DB connected: " + conn.State;
+});
+
 app.Run();
